1f0e0dfa3332fe33a21ae3371f80b0d4e6b0bcc8
Artyom Melnikov <a.melnikov@tegra.kz>
2023-06-12 20:03:07 +0600

    added Homework Sem1

---
+++ Homeworks/Sem1/Program.cs
@@ -0,0 +1,68 @@
+﻿// Task #2
+
+// System.Console.Write("insert the a number -> ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("insert the b number -> ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a < b)
+// {
+//     Console.WriteLine($"max number is {b} and min number is {a}");
+// }
+// else
+// {
+//     Console.WriteLine($"min number is {b} and max number is {a}");
+// }  
+
+// Task #4
+
+// System.Console.Write("insert the first number -> ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("insert the second number -> ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("insert the third number -> ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// if(a > b && a > c)
+//     {
+//      Console.WriteLine($"max number is {a}");
+//     }
+// else
+//     if (b > c)
+// {
+//     Console.WriteLine($"max number is {b}");
+// }
+// else
+// {
+//     Console.WriteLine($"max number is {c}");
+// }
+
+// Task #6
+
+// System.Console.Write("insert some number -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num % 2 ==0)
+// {
+//     Console.WriteLine($"the number {num} is even");
+// }
+// else
+// {
+//     Console.WriteLine($"the number {num} is odd");
+// }
+
+// Task #8
+
+System.Console.Write("insert the N number -> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while(i <= n)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
--- Seminars/Sem1/Program.cs
+++ Seminars/Sem1/Program.cs
@@ -1,3 +1,54 @@
-﻿System.Console.Write("insert the number the square of the number you want to see -> ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"the square of the number {num} -> {num*num}");
+﻿// System.Console.Write("insert the number the square of the number you want to see -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"the square of the number {num} -> {num*num}");
+
+//Task2
+// 5 25 - ?
+// 25 5 - ?
+
+// System.Console.Write("insert the first number -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("insert the second number -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad = num2 * num2;
+
+// if (num1 == quad)
+// {
+//     Console.WriteLine($"First number {num1} is the squad of the second number {num2}");
+// }
+// else
+// {
+//     Console.WriteLine($"First number {num1} isn't the squad of the second number {num2}");
+// }
+
+// Task 3
+
+// System.Console.Write("insert the N number -> ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int current = n*(-1);
+
+// while(current <= n)
+// {
+//     Console.Write(current + " ");
+//     current ++;
+// }
+
+// Task 4
+
+// Дается 3хзначное число. Нужно вывести последнюю цифру числа.
+
+// System.Console.Write("insert the 3rd digit number -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if( num >= 100 && num <= 999)
+// {
+//     System.Console.WriteLine($"the last figure of the number {num} -> {num % 10}");
+// }
+// else
+// {
+//     System.Console.WriteLine("invalid input");
+// }
+
+
