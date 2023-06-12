675a2f1f4ccbae2560b2116d16c6337d785c7777
Artyom Melnikov <a.melnikov@tegra.kz>
2023-06-12 16:02:34 +0600

    added Sem 1 with square of the number

---
+++ Seminars/Sem1/Program.cs
@@ -0,0 +1,3 @@
+﻿System.Console.Write("insert the number the square of the number you want to see -> ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"the square of the number {num} -> {num*num}");
