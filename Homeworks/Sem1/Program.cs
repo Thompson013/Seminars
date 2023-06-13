3f1a13bcc45d7c649e3c4f895c5d204649725e3f
Artyom Melnikov <a.melnikov@tegra.kz>
2023-06-13 17:01:26 +0600

    added eminars Sem2

--- Homeworks/Sem1/Program.cs
+++ Homeworks/Sem1/Program.cs
@@ -53,16 +53,16 @@
 
 // Task #8
 
-System.Console.Write("insert the N number -> ");
-int n = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("insert the N number -> ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int i = 1;
+// int i = 1;
 
-while(i <= n)
-{
-    if (i % 2 == 0)
-    {
-        Console.Write(i + ", ");
-    }
-    i++;
-}
+// while(i <= n)
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.Write(i + ", ");
+//     }
+//     i++;
+// }
