236557e8b53c9b7d5d300fdf1731d0545ac72663
Artyom Melnikov <a.melnikov@tegra.kz>
2023-06-29 16:25:26 +0600

    added homework sem6

---
+++ Homeworks/Sem6/Program.cs
@@ -0,0 +1,104 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] EnterArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = System.Console.WriteLine($"Enter {i + 1} element");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers (int[] array )
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+System.Console.WriteLine("Enter array of number from 1 to 100: ") ;
+int[] array;
+array = EnterArray(length);
+PrintArray(array);
+System.Console.WriteLine($" the quantity of elements greater than 0 -> {CountPositiveNumber(array)}");
+
+// Console.Write("Введите числа через запятую: ");
+// int[] numbers = StringToNum(Console.ReadLine());
+// PrintArray(numbers);
+// int sum = 0;
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > 0)
+//     {
+//         sum++;
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+// int[] StringToNum(string input)
+// {
+//     int count = 1;
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] == ',')
+//         {
+//             count++;
+//         }
+//     }
+
+//     int[] numbers = new int [count];
+//     int index = 0;
+
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         string temp = "";
+
+//         while (input [i] != ',')
+//         {
+//         if(i != input.Length - 1)
+//         {
+//             temp += input [i].ToString();
+//             i++;
+//         }
+//         else
+//         {
+//             temp += input [i].ToString();
+//             break;
+//         }
+//         }
+//         numbers[index] = Convert.ToInt32(temp);
+//         index++;
+//     }
+//     return numbers;
+// }
+
+
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[ ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.Write("]");
+// }
+
