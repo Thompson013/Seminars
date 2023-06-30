b314c23b40a01d8ef73f0b6a6e69754fcaef7dee
Artyom Melnikov <a.melnikov@tegra.kz>
2023-06-30 13:37:23 +0600

    added homework sem6

--- Homeworks/Sem6/Program.cs
+++ Homeworks/Sem6/Program.cs
@@ -2,103 +2,59 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-int[] EnterArray(int length)
-{
-    int[] array = new int[length];
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = System.Console.WriteLine($"Enter {i + 1} element");
-    }
-    return array;
-}
-
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        System.Console.WriteLine($"a[{i}] = {array[i]}");
-    }
-}
-
-int CountPositiveNumbers (int[] array )
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if(array[i] > 0)
-        {
-            count++;
-        }
-    }
-    return count;
-}
-
-System.Console.WriteLine("Enter array of number from 1 to 100: ") ;
-int[] array;
-array = EnterArray(length);
-PrintArray(array);
-System.Console.WriteLine($" the quantity of elements greater than 0 -> {CountPositiveNumber(array)}");
-
-// Console.Write("Введите числа через запятую: ");
-// int[] numbers = StringToNum(Console.ReadLine());
-// PrintArray(numbers);
-// int sum = 0;
-// for (int i = 0; i < numbers.Length; i++)
+// int[] EnterArray(int length)
 // {
-//     if (numbers[i] > 0)
+//     int[] array = new int[length];
+//     Random rand = new Random();
+//     for (int i = 0; i < array.Length; i++)
 //     {
-//         sum++;
+//         array[i] = rand.Next(-100, 101);;
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
 //     }
 // }
-// Console.WriteLine();
-// Console.WriteLine($"количество значений больше 0 = {sum}");
 
-
-// int[] StringToNum(string input)
+// int CountPositiveNumbers (int[] array )
 // {
-//     int count = 1;
-//     for (int i = 0; i < input.Length; i++)
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
 //     {
-//         if (input[i] == ',')
+//         if(array[i] > 0)
 //         {
 //             count++;
 //         }
 //     }
-
-//     int[] numbers = new int [count];
-//     int index = 0;
-
-//     for (int i = 0; i < input.Length; i++)
-//     {
-//         string temp = "";
-
-//         while (input [i] != ',')
-//         {
-//         if(i != input.Length - 1)
-//         {
-//             temp += input [i].ToString();
-//             i++;
-//         }
-//         else
-//         {
-//             temp += input [i].ToString();
-//             break;
-//         }
-//         }
-//         numbers[index] = Convert.ToInt32(temp);
-//         index++;
-//     }
-//     return numbers;
+//     return count;
 // }
 
+// int[] array;
+// array = EnterArray(10);
+// PrintArray(array);
+// System.Console.WriteLine($" the quantity of elements greater than 0 -> {CountPositiveNumbers(array)}");
 
-// void PrintArray(int[] array)
-// {
-//     Console.Write("[ ");
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write(array[i] + " ");
-//     }
-//     Console.Write("]");
-// }
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
 
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Enter a value of b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter a value of k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter a value of b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter a value of k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1) / (-k1 + k2);
+double y = k2 * x + b2;
+
+System.Console.Write($"The point of intersection of two straight lines -> ({x},{y})");
