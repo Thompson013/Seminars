77548252f41d33c3b95690c3c11b2dac7cb80d0b
Artyom Melnikov <a.melnikov@tegra.kz>
2023-07-05 16:23:34 +0600

    added seminars sem7

--- Seminars/Sem7/Program.cs
+++ Seminars/Sem7/Program.cs
@@ -217,64 +217,70 @@
 // на пересечении которых расположен наименьший элемент.
 // то есть удалаем и строку и столбец в которой находится этот элемент
 
-int[,] Create2DArray (int rows, int columns,int minValue,int maxValue)
-{
-int[,] array = new int[rows,columns];
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-        array[i, j] = new Random().Next(minValue, maxValue+1);
-        }
-    }
-    return array;
-}
+// int[,] Create2DArray (int rows, int columns,int minValue,int maxValue)
+// {
+// int[,] array = new int[rows,columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//         array[i, j] = new Random().Next(minValue, maxValue+1);
+//         }
+//     }
+//     return array;
+// }
 
-void Print2DArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)// array.GetLength(0) == количество строк в 2Д массиве
-    {
-        for( int j = 0 ; j < array.GetLength(1); j++)// array.GetLength(0) == количество столбцов в 2Д массиве
-        {
-        System.Console.Write(array[i,j] + " ");
-        }
-        System.Console.WriteLine();
-    }
-    System.Console.WriteLine();
-}
+// void Print2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)// array.GetLength(0) == количество строк в 2Д массиве
+//     {
+//         for( int j = 0 ; j < array.GetLength(1); j++)// array.GetLength(0) == количество столбцов в 2Д массиве
+//         {
+//         System.Console.Write(array[i,j] + " ");
+//         }
+//         System.Console.WriteLine();
+//     }
+//     System.Console.WriteLine();
+// }
 
-int[,] MinValue(int[,] array)
-{
-    if(array.GetLength(0) != array.GetLength(1))
-    {
-        System.Console.WriteLine("it isn't a squad massive");
-        return array;
-    }
-    else
-    {
-        for (int i = 0; i < array.GetLength(0) - 1; i++)
-        {
-            for (int j = i + 1; j < array.GetLength(1); j++)
-            {
-                int temp = array[i, j];
-                array[i, j] = array[j, i];
-                array[j, i] = temp;
+// int[,] MinValue(int[,] array)
+// {
+//     int minI = 0;
+//     int minJ = 0;
+//     int minimal = array[0, 0];
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if(array[i, j] <= minimal)
+//             {
+//             minimal = array[i, j];
+//             minI = i;
+//             minJ = j;
+//             }
+//         }
+//     }
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         array[i, minJ] = 0;
+//     }
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[minI, j] = 0;
+//     }
+//     return array;    
+// }
 
-            }
-        }
-    }
-    return array;
-}
+// Console.WriteLine("Enter number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter minimal value of array element: ");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter amaximal value of array element: ");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Enter number of rows: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter number of columns: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter minimal value of array element: ");
-int minValue = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter amaximal value of array element: ");
-int maxValue = Convert.ToInt32(Console.ReadLine());
+// int[,] myArray = Create2DArray(rows, columns, minValue, maxValue);
+// Print2DArray(myArray);
+// Print2DArray(MinValue(myArray));
 
-int[,] myArray = Create2DArray(rows, columns, minValue, maxValue);
-Print2DArray(myArray);
-Print2DArray(RowsToColumns(myArray));
