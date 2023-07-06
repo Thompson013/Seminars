8c322d02085a83fa99744cc7e13f673881462114
Artyom Melnikov <a.melnikov@tegra.kz>
2023-07-06 08:04:58 +0600

    added homework sem7 v2

--- Homeworks/Sem7/Program.cs
+++ Homeworks/Sem7/Program.cs
@@ -5,7 +5,7 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-// double[,] Create2DArray(int rows, int columns, int minValue, int maxValue)
+// double[,] Create2DArray(int rows, int columns)
 // {
 //     double[,] array = new double[rows, columns];
 //     for (int i = 0; i < rows; i++)
@@ -35,12 +35,9 @@
 // int rows = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Enter number of columns: ");
 // int columns = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Enter minimal value of array element: ");
-// int minValue = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Enter amaximal value of array element: ");
-// int maxValue = Convert.ToInt32(Console.ReadLine());
 
-// Print2DArray(Create2DArray(rows, columns, minValue, maxValue));
+// double[,] myArray = Create2DArray(rows, columns);
+// Print2DArray(myArray);
 
 // Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
@@ -50,3 +47,60 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
+
+int[,] Create2DArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(1, 9);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+void FindElement(int[,] array)
+{
+    int rows = 0;
+    int columns = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (rows < array.GetLength(0) && columns < array.GetLength(1)) 
+            {
+                Console.WriteLine(array[rows, columns]);
+            }
+            else
+            {
+                Console.WriteLine($"{rows}{columns} -> the number doesn't present in array");
+            } 
+        }
+    }
+}
+
+Console.WriteLine("Enter index of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter index of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = new int[6, 8];
+Create2DArray(rows, columns);
+Print2DArray(myArray);
+FindElement(myArray);
+System.Console.WriteLine($"the meaning of required element -> {myArray}");
