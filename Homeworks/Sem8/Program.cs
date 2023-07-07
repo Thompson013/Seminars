05bbca6747a417a8323415451b2e412046ed3ebd
Artyom Melnikov <a.melnikov@tegra.kz>
2023-07-07 17:46:07 +0600

    added homework sem8 v1

---
+++ Homeworks/Sem8/Program.cs
@@ -0,0 +1,129 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+// void Create2DArray(int[,] array2D)
+// {
+//     for (int i = 0; i <array2D.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array2D.GetLength(1); j++)
+//         {
+//             array2D[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// void Print2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// int[,] GetOrderArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < array.GetLength(1) - 1; k++)
+//             {
+//                 if(array[i, k] < array[i, k + 1])
+//                 {
+//                     int temp = array[i, k + 1];
+//                     array[i, k + 1] = array[i, k];
+//                     array[i, k] = temp;
+//                 }
+//             }
+//         }
+//     }
+//     return array;
+// }
+
+// Console.WriteLine("Enter number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = new int[rows, columns];
+// Create2DArray(array);
+// Print2DArray(array);
+// GetOrderArray(array);
+// System.Console.WriteLine();
+// Print2DArray(array);
+
+// Задайте прямоугольный двумерный массив. Напишите программу, которая будет 
+// находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер 
+// строки с наименьшей суммой элементов: 1 строка
+
+void Create2DArray(int[,] array2D)
+{
+    for (int i = 0; i <array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            array2D[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[] FindMinRow(int [,] numbers)
+{
+    int[] rowSum = new int[numbers.GetLength(0)];
+    for (int i = 0; i < numbers.GetLength(0); i++) 
+        {
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                rowSum[i] += numbers[i, j];
+            }
+            System.Console.WriteLine($"{i + 1} -> {rowSum[i]}");  
+        }
+    int minRowSum = rowSum[0];
+    int minRow = 1;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        if (rowSum[i] < minRowSum)
+        {
+            minRowSum = rowSum[i];
+            minRow = i + 1;
+        }
+    }
+    return minRow;
+}
+
+int[,] array = new int[4, 4];
+Create2DArray(array);
+Print2DArray(array);
+FindMinRow(array);
+System.Console.WriteLine($"Less sum in {row} line");
